d4f2bc020ee4c1640431fbd6aedbfff58ac897d5
Quvonchbek <147556696+IbragimovQuvonchbek@users.noreply.github.com>
2023-12-04 14:31:52 +0500

    fixed bugs

--- books/models.py
+++ books/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+class Books(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.PositiveIntegerField()
+    photo = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
--- books/views.py
+++ books/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Books
 
-# Create your views here.
+
+class HomePageView(ListView):
+    model = Books
+    template_name = 'home.html'
